26244f95b28325446dbed3e7434dddb43131c6dc
Cedric Altermatt <cedric.altermatt1@students.fhnw.ch>
2022-10-01 14:57:03 +0200

    Update jsdoc

--- prototype/pages/page.js
+++ prototype/pages/page.js
@@ -1,37 +1,52 @@
 export { Page }
 
 /**
- * IPage is an interface. TODO complete jsdoc
+ * IPage is an interface. The IPage interface is used to construct HTML pages
+ * that can be integrated into the navigation model of Kolibri.
+ * IPage declares the navigation lifecycle of an HTML page in Kolibri.
  *
  * @typedef IPage<T>
  * @template T
- * @property { () => HTMLDivElement }                     activate
- * @property { (currentContent: HTMLDivElement) => void } passivate
- * @property { () => void }                               initialize
+ * @property { () => void } initialize - a function that initializes the page content. The page projector is called exactly once if the page has not been initialized.
+ *              After the first initialization an error is thrown if this function is called.
+ * @property { () => HTMLDivElement } activate - a function that returns this pages content.
+ *              Only one page can be activated at a time. The activated page is displayed in the browser.
+ *              The page has to be initialized for this function to work. Otherwise, a placeholder will be returned.
+ * @property { (latestContentState: HTMLDivElement) => void } passivate - a function that takes the latestContentState as an input before passivating the page.
+ *              The passivation of the page removes it from display in the browser. The latestContentState is stored in the page.
  *
  */
 
 /**
- * Constructor for an IPage. TODO complete jsdoc
+ * Constructor for an IPage<T>
  *
- * @param { IPageProjector } pageProjector
- * @returns  IPage<T>
+ * @template T
+ * @param { !IPageProjector } pageProjector
+ * @returns IPage<T>
  * @constructor
  * @example
- * TODO add example
+ * const homePageProjector = PageProjector(homePageProjector);
+ * const page = Page(homePageProjector);
+ * page.initialize();
+ * page.activate();
+ * page.passivate(latestContentState);
  */
 const Page = pageProjector => {
+    let initialized = false;
     let content = document.createElement('div');
     content.innerText = "Empty page. Please initialize";
 
     return {
-        activate: () => content,
-        passivate: currentContent => {
-            content = currentContent;
-        },
         initialize: () => {
-            content = pageProjector.projectPage()
-        }
+            if (initialized) {
+                throw 'Page has already been initialized.';
+            } else {
+                content = pageProjector.projectPage();
+                initialized = true;
+            }
+        },
+        activate: () => content,
+        passivate: latestContentState => content = latestContentState,
         //TODO add addCss and removeCss functions
     }
 };
