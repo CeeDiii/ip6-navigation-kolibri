1277a64351399aae3b78c5ac512a154cae21d28b
Cedric Altermatt <cedric.altermatt1@students.fhnw.ch>
2022-10-01 11:53:57 +0200

    Implement base structure for page interface

---
+++ prototype/pages/page.js
@@ -0,0 +1,28 @@
+export { Page }
+
+/**
+ * IPage is an interface. TODO complete jsdoc
+ *
+ * @typedef IPage<T>
+ * @template T
+ * @property { () => void } activate
+ * @property { () => void } passivate
+ * @property { () => void } initialize
+ *
+ */
+
+/**
+ * Constructor for an IPage<T>. TODO complete jsdoc
+ *
+ * @template T
+ * @param { T } pageImplType
+ * @returns  IPage<T>
+ * @constructor
+ * @example
+ * TODO add example
+ */
+const Page = pageImplType => {
+
+
+};
+
