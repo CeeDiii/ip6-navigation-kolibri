edc4a605d096f23796f4a7cb65ec18bdcd9be3bb
Cedric Altermatt <cedric.altermatt1@students.fhnw.ch>
2022-10-01 15:01:01 +0200

    Update jsdoc

--- prototype/pages/pageProjector.js
+++ prototype/pages/pageProjector.js
@@ -1,20 +1,25 @@
 export { PageProjector }
 
 /**
- * IPageProjectorType is a projector for page content.
+ * IPageProjector is a projector for page content.
  *
  * @typedef IPageProjector
- * @property { () => HTMLDivElement } projectPage
+ * @property { () => HTMLDivElement } projectPage - a function that creates the HTMLDivElement that represents this view
+ *
  */
 
 /**
- * Constructor for an IPageProjector. TODO complete jsdoc
+ * Constructor for an IPageProjector.
  *
  * @param { () => HTMLDivElement } projectPageFunc
  * @returns  IPageProjector<T>
  * @constructor
  * @example
- * TODO add example
+ * const projectPageFunc = () => {
+ *     const content = document.createElement('div');
+ *     content.innerText = 'Hello World';
+ * }
+ * const pageProjector = PageProjector(projectPageFunc);
  */
 const PageProjector = projectPageFunc => ({
     projectPage: projectPageFunc
