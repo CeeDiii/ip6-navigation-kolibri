2a8a50331942776ffb3fd6c6bc2ce94d5767ed8b
Cedric Altermatt <cedric.altermatt1@students.fhnw.ch>
2022-12-10 12:11:09 +0100

    Add 403 forbidden page

---
+++ prototype/pages/403/forbiddenPageProjector.js
@@ -0,0 +1,80 @@
+export { ForbiddenPageProjector }
+/**
+ * @typedef PageProjectorType
+ * @property { () => void } projectPage
+ */
+
+/**
+ * @constructor
+ * @param { PageControllerType } pageController
+ * @param { !HTMLDivElement } pinToElement
+ * @param { !String } contentFilePath - relative to index.html!
+ * @returns { PageProjectorType }
+ */
+
+const ForbiddenPageProjector = (pageController, pinToElement, contentFilePath) => {
+    const pageWrapper = pinToElement;
+    const contentWrapper = document.createElement('div');
+
+    const initialize = () => {
+        const contentPromise = fetchPageContent(contentFilePath);
+        contentPromise.then(contentHtml => {
+            contentWrapper.innerHTML = contentHtml;
+
+            if (pageWrapper.firstChild === null) {
+                pageWrapper.append(contentWrapper);
+            } else {
+                pageWrapper.replaceChild(contentWrapper, pageWrapper.firstChild);
+            }
+        });
+    };
+
+    const projectPage = () => {
+        // initialize content on first call
+        if (contentWrapper.firstChild === null) {
+            initialize();
+        }
+        // bind content to document
+        if (pageWrapper.firstChild === null) {
+            pageWrapper.append(contentWrapper);
+        } else {
+            pageWrapper.replaceChild(contentWrapper, pageWrapper.firstChild);
+        }
+    };
+
+    const fetchPageContent = async (filePath) => {
+        try {
+            const response = await fetch(filePath, {
+                    headers: {
+                        'Content-Type': 'application/html',
+                        'Accept': 'application/html'
+                    }
+                }
+            );
+            if (response.ok) {
+                const content = await response.text();
+                return content;
+            } else {
+                throw new Error(`HTTP error: ${response.status}`);
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    pageController.onValueChanged(newValue => {
+        // add class for specific page styling
+        contentWrapper.classList.add(newValue);
+    });
+
+    pageController.onActiveChanged(active => {
+        if (active) {
+            projectPage();
+        }
+    });
+
+    return {
+        projectPage
+    }
+};
+
