d356e44f0caa410e03f32f1142edffccaf1db285
Cedric Altermatt <cedric.altermatt1@students.fhnw.ch>
2022-12-10 15:21:59 +0100

    Add tests for all controller functions

--- prototype/navigation/navigationControllerTest.js
+++ prototype/navigation/navigationControllerTest.js
@@ -1,19 +1,16 @@
-import {TestSuite} from "../kolibri/util/test.js";
-import {PageController} from "../pages/pageController.js";
-import {NavigationController} from "./navigationController.js";
+import {TestSuite} from '../kolibri/util/test.js';
+import {PageController} from '../pages/pageController.js';
+import {NavigationController} from './navigationController.js';
 
+const navigationSuite = TestSuite('navigationController');
 
-const navigationSuite = TestSuite("navigationController");
-
-
-
-navigationSuite.add("addPageController", assert => {
-    const homePageController = PageController("home", null);
+navigationSuite.add('addPageController', assert => {
+    const homePageController = PageController('home', null);
     const navigationController = NavigationController();
 
     assert.isTrue(navigationController.addPageController(homePageController));
 
-    assert.is(navigationController.getPageController('#home'), homePageController)
+    assert.is(navigationController.getPageController('#home'), homePageController);
 
     assert.isTrue(!navigationController.addPageController(homePageController));
 
@@ -22,42 +19,79 @@ navigationSuite.add("addPageController", assert => {
     assert.isTrue(!navigationController.addPageController(undefined));
 });
 
-navigationSuite.add("deletePageController", assert => {
-    const homePageController = PageController("home", null);
+navigationSuite.add('addErrorPageController', assert => {
+    const errorNotFoundController = PageController('notFound', null);
+    const navigationController = NavigationController();
+
+    assert.isTrue(navigationController.addErrorPageController('404', errorNotFoundController));
+
+    assert.is(navigationController.getPageController('#404'), errorNotFoundController);
+});
+
+navigationSuite.add('deletePageController', assert => {
+    const homePageController = PageController('home', null);
     const navigationController = NavigationController();
 
     navigationController.addPageController(homePageController);
 
-    assert.is(navigationController.getPageController('#home'), homePageController)
+    assert.is(navigationController.getPageController('#home'), homePageController);
 
     navigationController.deletePageController('#home');
 
     assert.is(navigationController.getPageController('#home'), undefined);
 });
 
-navigationSuite.add("setHomepage", assert => {
-    const homePageController = PageController("home", null);
+navigationSuite.add('setHomepage', assert => {
+    const homePageController = PageController('home', null);
     const navigationController = NavigationController();
 
     navigationController.addPageController(homePageController);
 
-    assert.is(navigationController.getHomePage(), '');
+    assert.is(navigationController.getHomePage(), '#');
 
-    navigationController.setHomePage('home');
+    navigationController.setHomePage(homePageController.getHash());
 
-    assert.is(navigationController.getHomePage(), 'home');
+    assert.is(navigationController.getHomePage(), '#home');
 });
 
-navigationSuite.add("onNavigationHashAdd", assert => {
-    const homePageController = PageController("home", null);
+navigationSuite.add('onNavigationHashAddAndDel', assert => {
+    const homePageController = PageController('home', null);
     const navigationController = NavigationController();
     let newHash;
+    let isDeleted = false;
 
     navigationController.onNavigationHashAdd(hash => newHash = hash);
+    navigationController.onNavigationHashDel(() => isDeleted = true);
 
     navigationController.addPageController(homePageController);
 
     assert.is(newHash, '#home');
+
+    navigationController.deletePageController(homePageController.getHash());
+
+    assert.isTrue(isDeleted);
+});
+
+navigationSuite.add('onWebsiteNameChanged', assert => {
+    const navigationController = NavigationController();
+    let name;
+
+    navigationController.onWebsiteNameChanged(newName => name = newName);
+
+    navigationController.setWebsiteName('Kolibri');
+
+    assert.is(name, 'Kolibri');
+});
+
+navigationSuite.add('onWebsiteLogoChanged', assert => {
+    const navigationController = NavigationController();
+    let logoPath;
+
+    navigationController.onWebsiteLogoChanged(newPath => logoPath = newPath);
+
+    navigationController.setWebsiteLogo('./logo/kolibri.png');
+
+    assert.is(logoPath, './logo/kolibri.png');
 });
 
 navigationSuite.run();
--- prototype/pages/pageControllerTest.js
+++ prototype/pages/pageControllerTest.js
@@ -1,10 +1,82 @@
-import {TestSuite} from "../kolibri/util/test.js";
-import {PageController} from "./pageController.js";
+import {TestSuite} from '../kolibri/util/test.js';
+import {PageController} from './pageController.js';
 
-const navigationSuite = TestSuite("pageController");
+const navigationSuite = TestSuite('pageController');
 
-navigationSuite.add("onActiveChanged", assert => {
-    const homePageController = PageController("home", null);
+navigationSuite.add('getPageContentControllers', assert => {
+    const controller1 = {};
+    const controller2 = {};
+    const dummyControllers = [controller1, controller2];
+
+    const homePageController = PageController('home', dummyControllers);
+
+    assert.is(homePageController.getPageContentControllers(), dummyControllers);
+    assert.is(homePageController.getPageContentControllers()[0], dummyControllers[0]);
+    assert.is(homePageController.getPageContentControllers()[1], dummyControllers[1]);
+});
+
+navigationSuite.add('getHash', assert => {
+    const homePageController = PageController('home', null);
+
+    assert.is(homePageController.getHash(), '#home');
+});
+
+navigationSuite.add('getValue', assert => {
+    const homePageController = PageController('home', null);
+
+    assert.is(homePageController.getValue(), 'home');
+
+    homePageController.setValue('HOME');
+
+    assert.is(homePageController.getValue(), 'HOME');
+});
+
+navigationSuite.add('getParent', assert => {
+    const homePageController = PageController('home', null);
+
+    assert.is(homePageController.getParent(), null);
+
+    const parentHomePageController = PageController('parentHome', null);
+    homePageController.setParent(parentHomePageController);
+
+    assert.is(homePageController.getParent(), parentHomePageController);
+});
+
+navigationSuite.add('getIsVisible', assert => {
+    const homePageController = PageController('home', null);
+
+    assert.isTrue(homePageController.getIsVisible());
+
+    homePageController.setIsVisible(false);
+
+    assert.isTrue(!homePageController.getIsVisible());
+});
+
+navigationSuite.add('getIsNavigational', assert => {
+    const homePageController = PageController('home', null);
+
+    assert.isTrue(homePageController.getIsNavigational());
+
+    homePageController.setIsNavigational(false);
+
+    assert.isTrue(!homePageController.getIsNavigational());
+});
+
+navigationSuite.add('onIconChanged', assert => {
+    const homePageController = PageController('home', null);
+    let changedIcon;
+
+    homePageController.onIconChanged(icon => changedIcon = icon);
+
+    assert.is(changedIcon, './navigation/icons/placeholder.svg');
+
+    homePageController.setIcon('./navigation/icons/house.svg');
+
+    assert.is(changedIcon, './navigation/icons/house.svg');
+});
+
+navigationSuite.add('onActiveChanged', assert => {
+    const homePageController = PageController('home', null);
     let isActive;
 
     homePageController.onActiveChanged(state => isActive = state);
@@ -18,23 +90,72 @@ navigationSuite.add("onActiveChanged", assert => {
     assert.isTrue(!isActive);
 });
 
-navigationSuite.add("getHash", assert => {
-    const homePageController = PageController("home", null);
+navigationSuite.add('onVisitedChanged', assert => {
+    const homePageController = PageController('home', null);
+    let isVisited;
 
-    assert.is(homePageController.getHash(), '#home');
+    homePageController.onVisitedChanged(state => isVisited = state);
+
+    assert.isTrue(!isVisited);
+
+    homePageController.setVisited(true);
+
+    assert.isTrue(isVisited);
 });
 
-navigationSuite.add("onIconChanged", assert => {
-    const homePageController = PageController("home", null);
-    let changedIcon;
+navigationSuite.add('onValueChanged', assert => {
+    const homePageController = PageController('home', null);
+    let value;
 
-    homePageController.onIconChanged(icon => changedIcon = icon);
+    homePageController.onValueChanged(newValue => value = newValue);
 
-    assert.is(changedIcon, 'placeholder');
+    assert.is(homePageController.getValue(), 'home');
 
-    homePageController.setIcon('house');
+    homePageController.setValue('about');
 
-    assert.is(changedIcon, 'house');
+    assert.is(homePageController.getValue(), 'about');
 });
+
+navigationSuite.add('onIsNavigationalChanged', assert => {
+    const homePageController = PageController('home', null);
+    let isNavigational;
+
+    homePageController.onIsNavigationalChanged(state => isNavigational = state);
+
+    assert.isTrue(isNavigational);
+
+    homePageController.setIsNavigational(false);
+
+    assert.isTrue(!isNavigational);
+});
+
+navigationSuite.add('onIsVisibleChanged', assert => {
+    const homePageController = PageController('home', null);
+    let isVisible;
+
+    homePageController.onIsVisibleChanged(state => isVisible = state);
+
+    assert.isTrue(isVisible);
+
+    homePageController.setIsVisible(false);
+
+    assert.isTrue(!isVisible);
+});
+
+navigationSuite.add('onParentChanged', assert => {
+    const homePageController = PageController('home', null);
+    let parent;
+
+    homePageController.onParentChanged(newParent => parent = newParent);
+
+    assert.is(homePageController.getParent(), null);
+
+    const parentHomePageController = PageController('parentHome', null);
+    homePageController.setParent(parentHomePageController);
+
+    assert.is(homePageController.getParent(), parent);
+});
+
+
 
 navigationSuite.run();
